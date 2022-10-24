02f5765f12461f683fab97adb320838ed8d5d6c6
Mark Bieganek <markbieganek@gmail.com>
2022-10-24 10:35:44 -0600

    Add comment on break the problem down

--- think-like-a-programmer/half-of-a-square/script.js
+++ think-like-a-programmer/half-of-a-square/script.js
@@ -3,4 +3,13 @@
 CONSTRAINTS:
 1. Outputs only # and \n using only `console.log('#')` and `console.log('\n')`
 
-*/
+BREAK THE PROBLEM DOWN:
+
+We need a way to output a single line of five `console.log('#')` with a 
+`console.log('\n')` on the end.
+
+    Start with what we know.
+    - we can use a loop to output multiple `console.log('#') 5 times.`
+    - we can output `console.log('\n')` after the loop has completed.
+
+    */
