060a5ad580d33351496589301b37df570dfc0dde
Mark Bieganek <markbieganek@gmail.com>
2022-10-24 21:41:40 -0600

    Refactor singleLine() to use recursion

--- think-like-a-programmer/half-of-a-square/script.js
+++ think-like-a-programmer/half-of-a-square/script.js
@@ -8,17 +8,25 @@ BREAK THE PROBLEM DOWN:
 We need a way to output a single line of five `console.log('#')` with a 
 `console.log('\n')` on the end.
 
-    Start with what we know.
+Start with what we know.
+
+Algorithm - Write a single line: 
     - we can use a loop to output multiple `console.log('#') 5 times.`
     - we can output `console.log('\n')` after the loop has completed.
+    OR
+    - we can also do this with recursion.
+        - function parameters provide the symbol to repeat, and the
+        total times to repeat it.
+        - use a recursive function call to duplicate the value of 
+        `symbol` a total of `count` times.
 
     */
 
-const singleLine = () => {
-    for (i = 1; i <=5; i++) {
-        console.log('#');
+const singleLine = (symbol, count) => {
+    if (count > 1) {
+        symbol += singleLine(symbol, count -1);
     };
-    console.log('\n');
+    return symbol;
 }
 
-singleLine();
+console.log(singleLine('#', 5));
