5d83857b9f70de13be06df30c9dbbedf9eade6ec
Mark Bieganek <markbieganek@gmail.com>
2022-10-27 09:11:32 -0600

    Fix createElement() multiple class support & add div

--- the-odin-project/foundations/dom-manipulation-and-events/create-html-elements/script.js
+++ the-odin-project/foundations/dom-manipulation-and-events/create-html-elements/script.js
@@ -3,15 +3,14 @@ const createElement = (elementType, classNames, textContent) => {
     const element = document.createElement(elementType);
     // Add classes.
     if (classNames) {
-        // If a list of class names was provided, in an array, add them.
-        if (Array.isArray(classNames)) {
-            for (const className of classNames) {
-                element.classList.add(className);
-            };
-        } else {
-            // Add the list of classNames as-is.
-            element.classList.add(classNames); 
-        }
+        // If a list of class names was provided, add them to the new element.
+        if (typeof(classNames) === "string") {
+            // Split the list into an array.
+            classNames = classNames.split(" ");
+        };
+        for (const className of classNames) {
+            element.classList.add(className);
+        };
     };               
     // Add text content, if included.
     element.textContent = textContent;
@@ -24,5 +23,8 @@ const container = document.querySelector('#container');
 // Add a **`<p>`** with red text that says “Hey I’m red!”
 container.appendChild(createElement('p', 'warning', "Hey I'm red!"));
 
-// an **`<h3>`** with blue text that says “I’m a blue h3!”
+// Add an **`<h3>`** with blue text that says “I’m a blue h3!”
 container.appendChild(createElement('h3', 'heading', "I'm a blue h3!"));
+
+// Add a **`<div>`** with a black border and pink background color.”
+container.appendChild(createElement('div', 'highlight bordered bordered--dark', "a"));
